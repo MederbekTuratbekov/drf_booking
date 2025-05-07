efc44c1bc43d8ace4da59f116b9ce90733ca3c5b
Admin <mrrneo@outlook.com>
2025-05-07 20:17:26 +0600

    added serializers.py

--- booking_pro/booking/serializers.py
+++ booking_pro/booking/serializers.py
@@ -7,3 +7,48 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = '__all__'
 
+class CountrySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+class CitySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+
+class ChoiceCitySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ChoiceCity
+        fields = '__all__'
+
+
+class HotelSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = '__all__'
+
+class HotelImagesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = HotelImages
+        fields = '__all__'
+
+class ApartmentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Apartment
+        fields = '__all__'
+
+class ApartmentImagesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ApartmentImages
+        fields = '__all__'
+
+class ReviewsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Reviews
+        fields = '__all__'
+
+class BookingSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
