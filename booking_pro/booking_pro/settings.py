de3b6f8b9b77432918e80b66c0ac94c235ba567d
Admin <mrrneo@outlook.com>
2025-05-18 13:47:46 +0600

    ...

--- booking_pro/booking_pro/settings.py
+++ booking_pro/booking_pro/settings.py
@@ -102,7 +102,7 @@ LANGUAGES = [
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
